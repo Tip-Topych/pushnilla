4ba27b74d82ceeb362d167368208d7fee51dc8aa
Tip-Topych <padla666@list.ru>
2022-07-07 17:49:13 +0700

    done template

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ from loguru import logger
 from aiogram.types import ParseMode
 from os import environ
 
-API_TOKEN: str = environ.get('API_TOKEN')    # 'AAABBBCCC:12345'
-ADMIN_ID: list = list(map(int, environ.get('ADMIN_ID').split()))    # [123, 456]
-BAN_LIST: list = list(map(int, environ.get('BAN_LIST').split()))    # [321, 654]
+API_TOKEN: str = environ.get('BOT_TOKEN')  # 'AAABBBCCC:12345'
+ADMIN_ID: str = environ.get('ADMIN_ID')  # [123, 456]
+# BAN_LIST: list = list(map(str, environ.get('BAN_LIST').split()))    # [321, 654]
 
 # HIDE_SOURCE = True
 DB_PATH = '_bot.db'
