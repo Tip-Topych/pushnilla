c69d9fac4ac297b461f084da4ccc7634ffd8aed9
Tip-Topych <padla666@list.ru>
2022-07-07 22:16:17 +0700

    done functional template

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 #!venv/bin/python
 
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 import asyncio
 import aioschedule
+import config
 from os import environ
 
-API_TOKEN: str = environ.get('BOT_TOKEN') # не забудь добавить переменную среды в .env
+API_TOKEN: str = environ.get('BOT_TOKEN')  # не забудь добавить переменную среды в .env
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
 
 dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
 
+if not config.BOT_TOKEN:
+    exit("No token provided")
+
 
 async def noon_print():
-    await bot.send_message(-1001608864323, 'Go to DAYLY, motherfucker')
+    await bot.send_message(-1001608864323, 'Go to DAILY, motherfucker')
 
 
 async def scheduler():
-    aioschedule.every().day.at("21:08").do(noon_print)
+    aioschedule.every(1).minutes.do(noon_print)
+    # aioschedule.every().hour.do(job)
+    aioschedule.every().day.at("21:40").do(noon_print)
+    # aioschedule.every().monday.do(job)
+    # aioschedule.every().wednesday.at("13:15").do(job)
+    # aioschedule.every().minute.at(":17").do(job)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
