0edbfc235eca1a71c19d2ac8b8bd775ef75d4997
Tip-Topych <padla666@list.ru>
2022-07-08 14:44:49 +0700

    remove chat id to env

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import logging
 from aiogram import Bot, Dispatcher, executor
 import asyncio
 import aioschedule
-import config
 import os
 
 API_TOKEN: str = os.getenv('BOT_TOKEN')  # не забудь добавить переменную среды в .env
+CHAT_ID: str = os.getenv('CHAT')  # не забудь добавить переменную среды в .env
 
 bot = Bot(token=API_TOKEN)
 
@@ -13,12 +13,9 @@ dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.DEBUG)
 
-if not config.BOT_TOKEN:
-    exit("No token provided")
-
 
 async def noon_print():
-    await bot.send_message(-1001608864323, 'Go to DAILY, motherfucker')
+    await bot.send_message(CHAT_ID, 'Go to DAILY, motherfucker')
 
 
 async def scheduler():
@@ -26,7 +23,7 @@ async def scheduler():
     # aioschedule.every().hour.do(job)
     aioschedule.every().day.at("10:08").do(noon_print)
     # aioschedule.every().monday.do(job)
-    # aioschedule.every().wednesday.at("13:15").do(job)
+    aioschedule.every().friday.at("15:01").do(noon_print)
     # aioschedule.every().minute.at(":17").do(job)
     while True:
         await aioschedule.run_pending()
