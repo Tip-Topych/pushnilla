581b0aecfddc45b6c43060eb53755dc455d5e797
Tip-Topych <padla666@list.ru>
2022-07-15 21:16:25 +0700

    update sending photo

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import asyncio
 import aioschedule
 import os
 
-
 API_TOKEN: str = os.getenv('BOT_TOKEN')  # не забудь добавить переменную среды в .env
 CHAT_ID: str = os.getenv('CHAT')  # не забудь добавить переменную среды в .env
 
@@ -12,11 +11,10 @@ bot = Bot(token=API_TOKEN)
 
 dp = Dispatcher(bot)
 
-
 logging.basicConfig(level=logging.DEBUG)
 
 
-async def noon_print():
+async def go_daily():
     await bot.send_message(chat_id=CHAT_ID, text='Пора на дейли')
 
 
@@ -31,21 +29,21 @@ async def task_close_image():
 
 
 async def scheduler():
-    aioschedule.every(5).minutes.do(noon_print)
-    aioschedule.every(3).minutes.do(task_close_image)
+    # aioschedule.every(5).minutes.do(noon_print)
+    # aioschedule.every(3).minutes.do(task_close_image)
     # aioschedule.every().hour.do(job)
-    aioschedule.every().day.at("12:15").do(noon_print)
+    #aioschedule.every().day.at("12:15").do(noon_print)
     # aioschedule.every().monday.do(job)
-    aioschedule.every().monday.at("06:55").do(noon_print)
-    aioschedule.every().monday.at("15:00").do(task_close)
-    aioschedule.every().tuesday.at("06:55").do(noon_print)
-    aioschedule.every().tuesday.at("15:00").do(task_close)
-    aioschedule.every().wednesday.at("06:55").do(noon_print)
-    aioschedule.every().wednesday.at("15:00").do(task_close)
-    aioschedule.every().thursday.at("06:55").do(noon_print)
-    aioschedule.every().thursday.at("15:00").do(task_close)
-    aioschedule.every().friday.at("06:55").do(noon_print)
-    aioschedule.every().friday.at("15:00").do(task_close)
+    aioschedule.every().monday.at("06:55").do(go_daily)
+    aioschedule.every().monday.at("15:00").do(task_close_image)
+    aioschedule.every().tuesday.at("06:55").do(go_daily)
+    aioschedule.every().tuesday.at("15:00").do(task_close_image)
+    aioschedule.every().wednesday.at("06:55").do(go_daily)
+    aioschedule.every().wednesday.at("15:00").do(task_close_image)
+    aioschedule.every().thursday.at("06:55").do(go_daily)
+    aioschedule.every().thursday.at("15:00").do(task_close_image)
+    aioschedule.every().friday.at("06:55").do(go_daily)
+    aioschedule.every().friday.at("15:00").do(task_close_image)
     # aioschedule.every().minute.at(":17").do(job)
     while True:
         await aioschedule.run_pending()
@@ -54,6 +52,7 @@ async def scheduler():
 
 async def on_startup(_):
     asyncio.create_task(scheduler())
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
